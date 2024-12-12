085b1deb00db94ea52cfcdf82964dc11174b869e
Ashu Gupta <61074670+ashugupta1@users.noreply.github.com>
2024-12-12 22:14:41 +0530

    ayuvya assignment

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,15 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+
+
+const page = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Link href='/'>home</Link>
+    </div>
+  );
+};
+
+export default page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,104 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+import "./globals.css";
 
-export default function Home() {
+const Page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      {/* Container for the images */}
+      <div className="h-96 w-3/4  bg-slate-900 rounded-3xl flex justify-between items-center group relative overflow-hidden">
+        {/* Image 1 */}
+        <div className=" h-full w-1/3 transition-all duration-500 ease-in-out hover:w-[200%]">
+          <div
+            className="h-full w-full bg-cover bg-center rounded-l-3xl brightness-90"
+            style={{ backgroundImage: "url('/strength.jpg')" }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <span className="flex items-between justify-between  ml-4 text-white text-[20px]  mr-4">
+              {" "}
+              STRENGTH{" "}
+              <svg
+                className="h-10 w-10 text-slate-500 cursor-pointer rounded-full mt-2"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="white"
+                radius="50%"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                {" "}
+                <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                <polyline points="9 6 15 12 9 18" />
+              </svg>
+            </span>
+            {/* <div className="text-white text-[20px] ml-2">STRENGTH</div>
+            <div className="flex items-end justify-end mr-3 mt-1">
+              
+            </div> */}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* Image 2 */}
+        <div className=" h-full w-1/3 transition-all duration-500 ease-in-out  hover:w-[200%] opacity-100 text-sm">
+          <div
+            className="h-full w-full bg-cover bg-center"
+            style={{ backgroundImage: "url('/mobility.jpg')" }}
+          >
+            <span className="flex items-between justify-between  ml-4 text-white text-[20px]  mr-4">
+              
+              MPBILITY
+              <svg
+                className="h-10 w-10 text-slate-500 cursor-pointer rounded-full mt-2"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="white"
+                radius="50%"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                {" "}
+                <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                <polyline points="9 6 15 12 9 18" />
+              </svg>
+            </span>
+          </div>
+        </div>
+
+        {/* Image 3 */}
+        <div className=" h-full w-1/3 transition-all duration-500 ease-in-out  hover:w-[200%]  text-white">
+          <div
+            className="h-full w-full bg-cover bg-center rounded-r-3xl brightness-90"
+            style={{ backgroundImage: "url('/drills.jpeg')" }}
+          >
+            <span className="flex items-between justify-between  ml-4 text-white text-[20px]  mr-4">
+              DRILLS
+              <svg
+                className="h-10 w-10 text-slate-500 cursor-pointer rounded-full mt-2"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="white"
+                radius="50%"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                {" "}
+                <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                <polyline points="9 6 15 12 9 18" />
+              </svg>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Page;
